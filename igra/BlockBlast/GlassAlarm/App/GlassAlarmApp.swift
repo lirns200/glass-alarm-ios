6f22f6ce60a676866483155e9871f7161bc2390c
lirns200 <sivmak93@gmail.com>
2026-06-01 22:28:08 +0300

    chore: clean up game code for build

--- igra/BlockBlast/GlassAlarm/App/GlassAlarmApp.swift
+++ igra/BlockBlast/GlassAlarm/App/GlassAlarmApp.swift
@@ -2,7 +2,6 @@ import SwiftUI
 
 @main
 struct GlassAlarmApp: App {
-    private static let buildUniqueId = "22cdf908cfc19770408ce8b1301e5f33"
     var body: some Scene {
         WindowGroup {
             ContentView()
