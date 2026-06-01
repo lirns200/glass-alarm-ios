778067f2e2b48795cbf76d205d320192ce4fed69
lirns200 <sivmak93@gmail.com>
2026-06-01 22:48:14 +0300

    chore: auto sync ipa build files (2026-06-01 22:48:14)

--- GlassAlarm/App/GlassAlarmApp.swift
+++ GlassAlarm/App/GlassAlarmApp.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 @main
 struct GlassAlarmApp: App {
-    private static let buildUniqueId = "8366fc4a6f88c87f759c8e6a1903f4fb"
+    private static let buildUniqueId = "b95614ee5f21a92a67f436efd229d844"
     var body: some Scene {
         WindowGroup {
             ContentView()
