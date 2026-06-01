ccdcc857d342bc6b8aa80d7e7a0dc85b90cfc207
lirns200 <sivmak93@gmail.com>
2026-06-01 22:18:05 +0300

    chore: auto sync ipa build files (2026-06-01 22:18:05)

--- igra/BlockBlast/GlassAlarm/App/GlassAlarmApp.swift
+++ igra/BlockBlast/GlassAlarm/App/GlassAlarmApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @main
 struct GlassAlarmApp: App {
+    private static let buildUniqueId = "b100491dea3b6ea90c3a8ff6d18ea1e3"
     var body: some Scene {
         WindowGroup {
             ContentView()
--- igra/BlockBlast/GlassAlarm/Views/ContentView.swift
+++ igra/BlockBlast/GlassAlarm/Views/ContentView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Foundation
 
 class IPChecker: ObservableObject {
     @Published var isYukuVPN: Bool = false
